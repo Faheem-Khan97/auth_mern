ed7cbc63eb546e637de090e83bd9eabac6d8cc44
faheem <faheemkhan4865@gmail.com>
2022-03-06 09:44:57 +0530

    Implementing auth using Signed cookies

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,48 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 const app = express();
 const PORT = 5000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser('1234-4353-9879-8438'));
 
 function basicAuth(req, res, next){
-    const authHead = req.headers.authorization;
-    if(!authHead){
-        const err = new Error("You need to authenticate first");
-        res.statusCode = 401;
-        res.set("WWW-Authenticate", "Basic");
-        return next(err)
-    }
 
-    const [username, password] = Buffer.from(req.headers.authorization.split(" ")[1], 'base64').toString().split(":");
-    if(username === 'admin' && password === "12345"){
-        res.statusCode = 200;
-        next();
+    console.log(req.signedCookies);
+
+    if(!req.signedCookies?.user){
+        const authHead = req.headers.authorization;
+        if(!authHead){
+            const err = new Error("You need to authenticate first");
+            res.statusCode = 401;
+            res.set("WWW-Authenticate", "Basic");
+            return next(err)
+        }
+        const [username, password] = Buffer.from(req.headers.authorization.split(" ")[1], 'base64').toString().split(":");
+        if(username === 'admin' && password === "12345"){
+            res.cookie('user', 'admin', {signed:true})
+            res.statusCode = 200;
+            next();
+        }
+        else{
+            const err = new Error("Username or Password didn't match");
+            res.statusCode = 401;
+            res.set("WWW-Authenticate", "Basic");
+            return next(err)
+        }
+
     }
     else{
-        const err = new Error("Username or Password didn't match");
-        res.statusCode = 401;
-        res.set("WWW-Authenticate", "Basic");
-        return next(err)
+        if(req.signedCookies.user === 'admin'){
+            next()
+        }
+        else{
+            const err = new Error("Username or Password didn't match");
+            res.statusCode = 401;
+            return next(err)
+        }
     }
-
+    
 }
 
 app.use(basicAuth);
